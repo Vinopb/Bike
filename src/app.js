d69b1e2cbdf569b51bd0e4730ab50ac6bfae0807
VinodPc <vinodpb@outlook.com>
2017-08-12 06:50:28 -0400

    fire base config

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import React,{Component}from 'react';
 import {Text,View} from 'react-native';
 import {Header} from './components/common';
+import  firebase from 'firebase';
 
 class App extends Component{
+
+    componentWillMount()
+    {
+        firebase.initializeApp({
+             apiKey: "AIzaSyCmZGZNqWhKTPCXb0XmVnJC-8lUG-gmpeI",
+             authDomain: "bike-ca91a.firebaseapp.com",
+             databaseURL: "https://bike-ca91a.firebaseio.com",
+             projectId: "bike-ca91a",
+             storageBucket: "bike-ca91a.appspot.com",
+             messagingSenderId: "980365312212"
+            });
+    }
    
     render (){
       return(
